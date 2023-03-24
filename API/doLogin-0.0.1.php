9d63b8645e2bc0ea1a549c70722a048b4dbbfb6d
bomxacalaka <jorgeeduardodsc@gmail.com>
2023-03-24 23:34:02 +0000

    bigchanges

--- API/doLogin-0.0.1.php
+++
@@ -1,6 +0,0 @@
-<?php
-$data = array(
-    "message" => "Placeholder for future API version of login"
-);
-$payload = json_encode(array("user" => $data));
-echo $payload;
--- android/app/src/main/kotlin/com/example/collab_ws/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.collab_ws
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
